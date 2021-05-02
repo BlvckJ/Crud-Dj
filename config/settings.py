acd250e19e3f7a371e2001b39a65c5fd5142ad4d
BlvckJ <ainadaniel102@gmail.com>
2021-05-01 17:09:40 -0700

    i cant kill myself

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l5y_&x*)x&(gd^2zyqgkwsoo04ve(xa(_f__f95%iu4gu)&+d0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [' .herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
